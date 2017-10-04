96359ed8583470a244bed79137e02a460609bd34
Tyler Coverstone <TylerCoverstone@Tylers-MacBook-Pro-4.local>
2017-10-04 15:56:43 -0400

    modal stuff

--- jqscript.js
+++ jqscript.js
@@ -81,7 +81,13 @@ $(document).ready(function() {
         });
     });
 
- 
+    var modal = document.getElementById('myModal');
+
+    window.onclick = function(event) {
+        if(event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
 
 });
 
